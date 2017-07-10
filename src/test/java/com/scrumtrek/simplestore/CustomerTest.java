df51752df55b5f4a3a13ac4b6afd5f5923fa6876
i1656928 <i1656928@mvrht.net>
2017-07-10 16:30:06 +0300

    some tests added

--- src/test/java/com/scrumtrek/simplestore/CustomerTest.java
+++ src/test/java/com/scrumtrek/simplestore/CustomerTest.java
@@ -29,6 +29,34 @@ public class CustomerTest {
         Assert.assertTrue("Amount owed need to be 0", statement.contains("Amount owed is 0"));
     }
 
+    @Test
+    public void itShouldMovieInfoWhenAddRental(){
+        String sutName = "Dummy customer";
+        Customer sut = new Customer(sutName);
+
+
+        String dummyMovieName = "Dummy movie";
+        Movie dummyMovie = new Movie(dummyMovieName, PriceCodes.Childrens);
+
+        // Create rentals
+        int dummyRentalDays = 5;
+        Rental dummyRental = new Rental(dummyMovie, dummyRentalDays);
+
+        // Assign rentals to customers
+        sut.addRental(dummyRental);
+
+        String statement = sut.Statement();
+
+
+        // System.out.println(statement);
+
+        Assert.assertTrue("Statement need contain customer", statement.contains(sutName));
+        Assert.assertTrue("1 point need to be earned", statement.contains("earned 1 frequent renter points"));
+        Assert.assertTrue("Amount owed need not to be 0", !statement.contains("Amount owed is 0"));
+        Assert.assertTrue("Amount owed need to be in statement", statement.contains("Amount owed is"));
+        Assert.assertTrue("Movie name need to be in statement", statement.contains(dummyMovieName));
+    }
+
 
 
 
