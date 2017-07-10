c0cd1091f1b8916a5eb45ef2cfd82f0a95bc58bb
i1656928 <i1656928@mvrht.net>
2017-07-10 16:40:21 +0300

    some tests added

--- src/test/java/com/scrumtrek/simplestore/CustomerTest.java
+++ src/test/java/com/scrumtrek/simplestore/CustomerTest.java
@@ -30,7 +30,7 @@ public class CustomerTest {
     }
 
     @Test
-    public void itShouldMovieInfoWhenAddRental(){
+    public void itShouldMovieInfoWhenAddRentalForChildren(){
         String sutName = "Dummy customer";
         Customer sut = new Customer(sutName);
 
@@ -48,12 +48,42 @@ public class CustomerTest {
         String statement = sut.Statement();
 
 
-        // System.out.println(statement);
+
+         System.out.println(statement);
 
         Assert.assertTrue("Statement need contain customer", statement.contains(sutName));
         Assert.assertTrue("1 point need to be earned", statement.contains("earned 1 frequent renter points"));
-        Assert.assertTrue("Amount owed need not to be 0", !statement.contains("Amount owed is 0"));
-        Assert.assertTrue("Amount owed need to be in statement", statement.contains("Amount owed is"));
+
+        Assert.assertTrue("Amount owed need to be in statement", statement.contains("Amount owed is 3"));
+        Assert.assertTrue("Movie name need to be in statement", statement.contains(dummyMovieName));
+    }
+
+    @Test
+    public void itShouldMovieInfoWhenAddRentalForRegular(){
+        String sutName = "Dummy customer";
+        Customer sut = new Customer(sutName);
+
+
+        String dummyMovieName = "Dummy movie";
+        Movie dummyMovie = new Movie(dummyMovieName, PriceCodes.Regular);
+
+        // Create rentals
+        int dummyRentalDays = 5;
+        Rental dummyRental = new Rental(dummyMovie, dummyRentalDays);
+
+        // Assign rentals to customers
+        sut.addRental(dummyRental);
+
+        String statement = sut.Statement();
+
+
+
+         System.out.println(statement);
+
+        Assert.assertTrue("Statement need contain customer", statement.contains(sutName));
+        Assert.assertTrue("1 point need to be earned", statement.contains("earned 1 frequent renter points"));
+
+        Assert.assertTrue("Amount owed need to be 6.5 in statement", statement.contains("Amount owed is 6.5"));
         Assert.assertTrue("Movie name need to be in statement", statement.contains(dummyMovieName));
     }
 
