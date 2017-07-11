cb8782df53a97fb7b988929aa48e7e0d89c24532
i1656928 <i1656928@mvrht.net>
2017-07-11 15:26:55 +0300

    some tests added

--- src/test/java/com/scrumtrek/simplestore/CustomerTest.java
+++ src/test/java/com/scrumtrek/simplestore/CustomerTest.java
@@ -132,6 +132,39 @@ public class CustomerTest {
     }*/
 
     @Test
+    public void itShouldMovieInfoWhenAddRentalForRegular(){
+        String sutName = "Dummy customer";
+        Rental stubRental = mock(Rental.class);
+        Movie stubMoview = mock(Movie.class);
+
+        when(stubRental.getMovie()).thenReturn(stubMoview);
+        when(stubRental.getDaysRented()).thenReturn(5);
+        when(stubMoview.getPriceCode()).thenReturn(PriceCodes.Regular);
+        when(stubMoview.getTitle()).thenReturn("Stub moview");
+
+        Customer sut = new Customer(sutName, new PriceCalculator());
+
+
+        // Create rentals
+
+        // Assign rentals to customers
+        sut.addRental(stubRental);
+
+        String statement = sut.Statement();
+
+
+
+        System.out.println(statement);
+
+        Assert.assertTrue("Statement need contain customer", statement.contains(sutName));
+        Assert.assertTrue("1 point need to be earned", statement.contains("earned 1 frequent renter points"));
+
+        Assert.assertTrue("Amount owed need to be 6.5 in statement", statement.contains("Amount owed is 6.5"));
+        Assert.assertTrue("Movie name need to be in statement", statement.contains("Stub movie"));
+    }
+
+
+    @Test
     public void itShouldMovieInfoWhenAddRentalForChildren(){
         String sutName = "Dummy customer";
 
